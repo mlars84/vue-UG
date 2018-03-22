2a4b0cfb48d8b457f4d5fd85a3a86912e2ac3e9f
Matt Larson <matt.a.larson@gmail.com>
2018-03-22 09:58:55 -0500

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 v-once>{{ title }}</h1>
+    <p>{{ sayHello() }}</p>
+    <a :href="link">Google</a>
+    <p v-html="finishedLink"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'hello',
+      link: 'http://google.com',
+      finishedLink: '<a href="http://google.com">Google</a>'
+    }
+  },
+  methods: {
+    sayHello () {
+      return this.title
+    }
+  }
+}
+</script>
+
+<style>
+</style>
